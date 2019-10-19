eafb46b1734e93e7e3bea49ca567801e2973ed61
mbevilacqua1982 <marco.bevilacqua@flyertech.it>
2019-10-19 02:01:54 +0200

    Added subpackages and custom post format

--- wp-content/themes/pci/functions.php
+++ wp-content/themes/pci/functions.php
@@ -11,4 +11,9 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+add_action( 'after_setup_theme', 'childtheme_formats', 11 );
+function childtheme_formats(){
+     add_theme_support( 'post-formats', array( 'aside', 'gallery', 'custom' ) );
+}
 ?>
