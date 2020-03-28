3202242b25ea8979ddef7a33eb09972b17623db6
mbevilacqua1982 <marco.bevilacqua@flyertech.it>
2020-03-28 02:09:21 +0100

    Post style initial commit

--- wp-content/themes/pci/template-parts/post/content-custom.php
+++ wp-content/themes/pci/template-parts/post/content-custom.php
@@ -22,23 +22,12 @@
 		<?php
 
 		if ( is_single() ) {
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h1 class="entry-title pci-post-title">', '</h1>' );
 		} elseif ( is_front_page() && is_home() ) {
 			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 		} else {
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		}
-
-		if ( 'post' === get_post_type() ) {
-			echo '<div class="entry-meta">';
-			if ( is_single() ) {
-				twentyseventeen_posted_on();
-			} else {
-				echo twentyseventeen_time_link();
-				twentyseventeen_edit_link();
-			};
-			echo '</div><!-- .entry-meta -->';
-		};
 		?>
 	</header><!-- .entry-header -->
 
@@ -50,34 +39,45 @@
 		</div><!-- .post-thumbnail -->
 	<?php endif; ?>
 
-	<div class="entry-content">
+	<div class="entry-content entry-content-pci">
 		<div class="post-text">
-			<?php the_content(  ) ?>
+			<div class="entry-content-meta-container">
+				<div class="entry-content-meta-left">
+				<?php 
+					if ( 'post' === get_post_type() ) {
+						echo '<div class="entry-meta">';
+						if ( is_single() ) {
+							twentyseventeen_posted_on();
+						} else {
+							echo twentyseventeen_time_link();
+							twentyseventeen_edit_link();
+						};
+						echo '</div><!-- .entry-meta -->';
+					};
+				?>	
+				</div>
+				<div class="entry-content-meta-right">
+					<h5>
+						<?php echo get_field('data', false, false) ?>
+					</h5>			
+				</div>						
+			</div>			
+				
+			<?php the_content(  ) ?>		
+			
+			<div class="entry-content-footer"><!-- .entry-content-below -->
+			<div class="footer">
+				<h3 class="date-title"><i class="fa fa-2x fa-calendar"></i>Date</h3>
+				<?php 
+					$dates = explode(",", get_field('data', false, false));				
+					foreach ($dates as $key => $date) {				
+						echo "<span class=\"date-single\">" . $date . "</span>";
+					}			
+				?>
+			</div>				
+		</div><!-- .entry-content-footer --> 	
 		</div>
-		<div class="post-img">
-			<?php 
-				if ( ( is_single() || ( is_page() && ! twentyseventeen_is_frontpage() ) ) && has_post_thumbnail( get_queried_object_id() ) ) :
-					echo '<div class="single-featured-image-header">';
-					echo get_the_post_thumbnail( get_queried_object_id());
-					echo '</div><!-- .single-featured-image-header -->';
-				endif;
-			?>
-		</div>
-		<?php
-		?>
 	</div><!-- .entry-content -->
-	<div class="entry-content-footer"><!-- .entry-content-below -->
-		<div class="footer">
-			<h3 class="date-title"><i class="fa fa-2x fa-calendar"></i>Date</h3>
-			<?php 
-				$dates = explode(",", get_field('data', false, false));				
-				foreach ($dates as $key => $date) {				
-					echo "<span class=\"date-single\">" . $date . "</span>";
-				}			
-			?>
-		</div>				
-	</div><!-- .entry-content-below --> 
-
 	<?php
 	if ( is_single() ) {
 		twentyseventeen_entry_footer();
